4da23fe1a679caa2c0d5b63d6bace9d8483faeb5
muktashimbillah <muktasim.elfatih@gmail.com>
2026-04-30 17:10:38 +0700

    perbaikan huruf kapital pemanggilan pengaturan

--- app/Models/pengaturan.php
+++ app/Models/pengaturan.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class pengaturan extends Model
+class Pengaturan extends Model
 {
     protected $table = 'pengaturans';
 
--- database/seeders/PengaturanSeeder.php
+++ database/seeders/PengaturanSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Document;
-use App\Models\pengaturan;
+use App\Models\Pengaturan;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
